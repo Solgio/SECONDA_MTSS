376c4e9bf30faaf64085491083e043b7d03292de
PietroGiulioBassi <bassipietrogiulio@gmail.com>
2025-05-08 12:58:09 +0200

    CONVERT6 TDD (Expected to pass) #4

--- roman-number/src/main/java/it/unipd/mtss/IntegerToRoman.java
+++ roman-number/src/main/java/it/unipd/mtss/IntegerToRoman.java
@@ -6,16 +6,20 @@ package it.unipd.mtss;
 public class IntegerToRoman {
   public static String convert(int number){
     StringBuilder romanNumber = new StringBuilder();
-  String c ="I";
-  if (number < 1 || number > 3) {
-    throw new IllegalArgumentException("Number must be between 1 and 3");
+  if (number < 1 || number > 6) {
+    throw new IllegalArgumentException("Number must be between 1 and 6");
 }
 else {
-    for (int i = 0; i < number; i++) {;
-    
+  if(number <= 3 && number >=1){
+    for (int i = 0; i < number; i++) {
         romanNumber.append("I"); 
       }}
-      
-      return romanNumber.toString();
+  if (number == 4) {
+    romanNumber.append("IV");}
+  if (number == 5) {
+    romanNumber.append("V");}
+  if (number == 6) {
+    romanNumber.append("VI");}
   }
-}
+  return romanNumber.toString();
+}}
