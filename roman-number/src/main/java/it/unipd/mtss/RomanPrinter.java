762c437c86ee8a2a7f9a71f34b4fc0e60a11a09a
Solgio <lorenzosoligo04@gmail.com>
2025-05-08 12:45:10 +0200

    Print 3 TDD (Refactoring) close #3

--- roman-number/src/main/java/it/unipd/mtss/RomanPrinter.java
+++ roman-number/src/main/java/it/unipd/mtss/RomanPrinter.java
@@ -3,37 +3,43 @@
 // LORENZO SOLIGO 2101057
 ////////////////////////////////////////////////////////////////////
 package it.unipd.mtss;
+
 public class RomanPrinter {
     public static String print(int num){
+        if (num < 1 || num > 3) {
+            throw new IllegalArgumentException("Number must be between 1 and 3");
+        }
         return printAsciiArt(IntegerToRoman.convert(num));
     }
     private static String printAsciiArt(String romanNumber){
+
         StringBuilder asciiArt = new StringBuilder();
-        switch (romanNumber) {
-            case "I":
-                asciiArt.append("  ___ \n");
-                asciiArt.append(" |_ _|\n");
-                asciiArt.append("  | | \n");
-                asciiArt.append("  | | \n");
-                asciiArt.append(" |___|\n");
-                break;
-            case "II":
-                asciiArt.append("  ___   ___ \n");
-                asciiArt.append(" |_ _| |_ _|\n");
-                asciiArt.append("  | |   | | \n");
-                asciiArt.append("  | |   | | \n");
-                asciiArt.append(" |___| |___|\n");
-                break;
-            case "III":
-                asciiArt.append("  ___   ___   ___ \n");
-                asciiArt.append(" |_ _| |_ _| |_ _|\n");
-                asciiArt.append("  | |   | |   | | \n");
-                asciiArt.append("  | |   | |   | | \n");
-                asciiArt.append(" |___| |___| |___|\n");
-                break;
-            default:
-                throw new IllegalArgumentException("Invalid Roman numeral: " + romanNumber);
+        String[][] asciiI = {
+
+            {
+                "  ___ "      // I
+            },
+            {
+                " |_ _|"
+            },
+            {   
+                "  | | "
+            },
+            {
+                "  | | "
+            },
+            {
+                " |___|"
+            }
+        };
+
+        for (int i = 0; i < 5; i++) {
+            for( int j = 0; j < romanNumber.length(); j++) {
+                asciiArt.append(asciiI[i][0]);
+            }
+            asciiArt.append("\n");
         }
         return asciiArt.toString();
     }
+    
 }
