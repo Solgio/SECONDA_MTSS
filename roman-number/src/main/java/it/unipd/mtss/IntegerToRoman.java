e4ab22ee21a21fb4bc680ebe47ce8094ceecae27
PietroGiulioBassi <bassipietrogiulio@gmail.com>
2025-05-08 12:18:12 +0200

    CONVERT3 TDD (Expected to pass) #2

--- roman-number/src/main/java/it/unipd/mtss/IntegerToRoman.java
+++ roman-number/src/main/java/it/unipd/mtss/IntegerToRoman.java
@@ -4,8 +4,18 @@
 ////////////////////////////////////////////////////////////////////
 package it.unipd.mtss;
 public class IntegerToRoman {
-    public static String convert(int number){
-        // TODO
-        return null;
+  public static String convert(int number){
+  String c ="I";
+  if (number < 1 || number > 3) {
+    throw new IllegalArgumentException("Number must be between 1 and 3");
+}
+    if (number == 1) {
+        c = "I";
+    } else if (number == 2) {
+        c = "II";
+    } else if (number == 3) {
+        c = "III";
     }
+  return c;
+  }
 }
