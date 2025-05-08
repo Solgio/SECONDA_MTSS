0864441b42e7e41a51e5dd6e852bd254c77f753b
PietroGiulioBassi <bassipietrogiulio@gmail.com>
2025-05-08 15:12:53 +0200

    CONVERT500 (Expected to pass) No Refactoring needed close #14

--- roman-number/src/main/java/it/unipd/mtss/IntegerToRoman.java
+++ roman-number/src/main/java/it/unipd/mtss/IntegerToRoman.java
@@ -6,11 +6,11 @@ package it.unipd.mtss;
 public class IntegerToRoman {
   public static String convert(int number) {
     StringBuilder romanNumber = new StringBuilder();
-    if (number < 1 || number > 100) {
-        throw new IllegalArgumentException("Number must be between 1 and 100");
+    if (number < 1 || number > 500) {
+        throw new IllegalArgumentException("Number must be between 1 and 500");
     } else {
-        String[] romanSymbols = {"C","XC","L","XL","X", "IX", "V", "IV", "I"}; 
-        int[] integerValues = {100,90,50,40,10, 9, 5, 4, 1};    
+        String[] romanSymbols = {"D","CD","C","XC","L","XL","X", "IX", "V", "IV", "I"}; 
+        int[] integerValues = {500,400,100,90,50,40,10, 9, 5, 4, 1};    
         for (int i = 0; i < integerValues.length; i++) {
           while (number >= integerValues[i]) {
               romanNumber.append(romanSymbols[i]);
