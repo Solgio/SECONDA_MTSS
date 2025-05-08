73b845758bbb571f7d71c45d27d402c8a4e71ff9
PietroGiulioBassi <bassipietrogiulio@gmail.com>
2025-05-08 13:26:04 +0200

    CONVERT10 TDD (Refactoring) close #6

--- roman-number/src/main/java/it/unipd/mtss/IntegerToRoman.java
+++ roman-number/src/main/java/it/unipd/mtss/IntegerToRoman.java
@@ -4,31 +4,19 @@
 ////////////////////////////////////////////////////////////////////
 package it.unipd.mtss;
 public class IntegerToRoman {
-  public static String convert(int number){
+  public static String convert(int number) {
     StringBuilder romanNumber = new StringBuilder();
-  if (number < 1 || number > 10) {
-    throw new IllegalArgumentException("Number must be between 1 and 10");
-}
-else {
-  if(number <= 3){
-    for (int i = 0; i < number; i++) {
-        romanNumber.append("I"); 
-      }}
-  if (number == 4) {
-    romanNumber.append("IV");}
-  if (number == 5) {
-    romanNumber.append("V");}
-  if (number == 6) {
-    romanNumber.append("VI");}
-  
-  if (number == 7) {
-    romanNumber.append("VII");}
-  if (number == 8) {
-    romanNumber.append("VIII");}
-  if (number == 9) {
-    romanNumber.append("IX");}
-  if (number == 10) {
-    romanNumber.append("X");}
+    if (number < 1 || number > 10) {
+        throw new IllegalArgumentException("Number must be between 1 and 10");
+    } else {
+        String[] romanSymbols = {"X", "IX", "V", "IV", "I"}; 
+        int[] integerValues = {10, 9, 5, 4, 1};    
+        for (int i = 0; i < integerValues.length; i++) {
+          while (number >= integerValues[i]) {
+              romanNumber.append(romanSymbols[i]);
+              number -= integerValues[i];
+          }
+      }
+      return romanNumber.toString();
   }
-  return romanNumber.toString();
 }}
